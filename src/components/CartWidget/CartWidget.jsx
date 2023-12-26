973965ebf45108f51e90f2613dae9d2c08a3afff
EmilianaABaglioni <emi.baglioni@outlook.com>
2023-12-25 23:52:48 -0300

    nav-bar

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import './CartWidget.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 
 function CartWidget (){
     return (
         <>
-            <div>
-                <FontAwesomeIcon icon={faCartShopping} />
+            <div className='cartWidget'>
+                <FontAwesomeIcon className='icon-cart' icon={faCartShopping} />
             </div>
         </>
     )
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,9 +1,26 @@
 import CartWidget from '../CartWidget/CartWidget.jsx';
+import logo from './LOGO-MITHRA.png';
 
 function NavBar (){
     return (
         <>
-            <CartWidget/>
+            <div>
+                <div className='logo'>
+                    <img src={logo} alt="logo" />
+                </div>
+                <div>
+                     VER TODOS LOS PRODUCTOS
+                </div>
+                <div>
+                    SALE
+                </div>
+                <div>
+                    TABLA DE TALLES
+                </div>
+                <div>
+                    <CartWidget/>
+                </div>
+            </div>
         </>
     )
 };
